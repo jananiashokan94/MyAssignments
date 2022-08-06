a3303b30336476f852bb6d0687e2db9728b9f0de
jananiashokan94 <110736125+jananiashokan94@users.noreply.github.com>
2022-08-06 22:51:04 +0530

    Twowheeler

---
+++ day1/TwoWheeler.java
@@ -0,0 +1,31 @@
+package week1.day1;
+
+public class TwoWheeler {
+	
+	int noofwheels = 2;
+	short noOfMirrors = 2;
+	long chassisNumber = 123456l;
+	boolean isPuncnctured = false; 
+	String bikename = "pulsor";
+	double runningKM = 40.5 ;
+	
+	public static void main(String[] args)
+	{
+		TwoWheeler tw = new TwoWheeler();
+		System.out.println(tw.noofwheels);
+		System.out.println(tw.noOfMirrors);
+		System.out.println(tw.chassisNumber);
+		System.out.println(tw.isPuncnctured);
+		System.out.println(tw.bikename);
+		System.out.println(tw.runningKM);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
