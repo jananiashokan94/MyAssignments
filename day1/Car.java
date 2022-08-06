1638b6f5096981cdb7ee6ac05a1d60847ddcc256
jananiashokan94 <110736125+jananiashokan94@users.noreply.github.com>
2022-08-06 21:50:00 +0530

    day1

---
+++ day1/Car.java
@@ -0,0 +1,14 @@
+package Week1.day1;
+
+public class Car 
+{
+byte eminum = -30;
+int ref = 35000;
+char alpha = 'A' ;
+boolean isEmpty = true;
+long inf = 687345621;
+float pros = 638197293f;
+short Product = -5776;
+String sheet = "cotton";
+
+}
